0474d92bd1f3eb9c4a412a1f5b76fc95eaf564b5
priyan <saravanapriyan@satvatinfosol.com>
2018-02-23 18:00:10 +0530

    manifest errors cleared

--- financy/js/scripts.js
+++ financy/js/scripts.js
@@ -8,18 +8,23 @@ localforage.setDriver([
 
 		localforage.getItem(key).then(function(readValue) {
 			jsonStore=readValue;
-			for(i=0;i<readValue.length;i++){
-				htmlConsole.innerHTML += '<li>' + readValue[i].desc + '<span class="amount">' + readValue[i].amount + '</span></li>';
+			if(readValue){
+				for(i=0;i<readValue.length;i++){
+					htmlConsole.innerHTML += '<li>' + readValue[i].desc + '<span class="amount">' + readValue[i].amount + '</span></li>';
+				}
 			}
 		});
 
 	});
 	function saveEntry(){
+		if(jsonStore==null) jsonStore=[];
 		var value = {'desc':document.getElementById('txtEntry').value, 'amount':document.getElementById('numAmount').value};
 		jsonStore.push(value);
 		localforage.setItem(key, jsonStore, function() {
 			localforage.getItem(key).then(function(readValue) {
-				htmlConsole.innerHTML += '<li>' + readValue[readValue.length-1].desc + '<span class="amount">' + readValue[readValue.length-1].amount + '</span></li>';
+				if(readValue){
+					htmlConsole.innerHTML += '<li>' + readValue[readValue.length-1].desc + '<span class="amount">' + readValue[readValue.length-1].amount + '</span></li>';
+				}
 			});
 
 		});
