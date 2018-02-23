dee509ab902ec783be1cb619f48979ddefc8b0e2
priyan <saravanapriyan@satvatinfosol.com>
2018-02-23 17:50:20 +0530

    Primary design completed

---
+++ financy/js/scripts.js
@@ -0,0 +1,26 @@
+var key='STORE_KEY', jsonStore=[];
+
+localforage.setDriver([
+	localforage.INDEXEDDB,
+	localforage.WEBSQL,
+	localforage.LOCALSTORAGE
+	]).then(function() {
+
+		localforage.getItem(key).then(function(readValue) {
+			jsonStore=readValue;
+			for(i=0;i<readValue.length;i++){
+				htmlConsole.innerHTML += '<li>' + readValue[i].desc + '<span class="amount">' + readValue[i].amount + '</span></li>';
+			}
+		});
+
+	});
+	function saveEntry(){
+		var value = {'desc':document.getElementById('txtEntry').value, 'amount':document.getElementById('numAmount').value};
+		jsonStore.push(value);
+		localforage.setItem(key, jsonStore, function() {
+			localforage.getItem(key).then(function(readValue) {
+				htmlConsole.innerHTML += '<li>' + readValue[readValue.length-1].desc + '<span class="amount">' + readValue[readValue.length-1].amount + '</span></li>';
+			});
+
+		});
+	}
