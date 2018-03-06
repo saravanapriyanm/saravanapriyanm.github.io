2562d06e5814731436572cbff5fa0e3f9dc419ee
priyan <saravanapriyan@satvatinfosol.com>
2018-03-06 10:29:40 +0530

    Angularjs implemented for adding, listing

--- financy/js/scripts.js
+++ financy/js/scripts.js
@@ -1,31 +1,44 @@
-var key='STORE_KEY', jsonStore=[];
+lapp = angular.module('listApp', []);
+lapp.controller('listController', function($scope,$http,$timeout) {
 
-localforage.setDriver([
-	localforage.INDEXEDDB,
-	localforage.WEBSQL,
-	localforage.LOCALSTORAGE
-	]).then(function() {
+	$scope.key='STORE_KEY', $scope.jsonStore=[], $scope.value={};
 
-		localforage.getItem(key).then(function(readValue) {
-			jsonStore=readValue;
-			if(readValue){
-				for(i=0;i<readValue.length;i++){
-					htmlConsole.innerHTML += '<li>' + readValue[i].desc + '<span class="amount">' + readValue[i].amount + '</span></li>';
-				}
-			}
-		});
-
-	});
-	function saveEntry(){
-		if(jsonStore==null) jsonStore=[];
-		var value = {'desc':document.getElementById('txtEntry').value, 'amount':document.getElementById('numAmount').value};
-		jsonStore.push(value);
-		localforage.setItem(key, jsonStore, function() {
-			localforage.getItem(key).then(function(readValue) {
+	$scope.init = function(){
+		localforage.setDriver([
+			localforage.INDEXEDDB,
+			localforage.WEBSQL,
+			localforage.LOCALSTORAGE
+			]).
+		then(function() {
+			localforage.getItem($scope.key).then(function(readValue) {
 				if(readValue){
-					htmlConsole.innerHTML += '<li>' + readValue[readValue.length-1].desc + '<span class="amount">' + readValue[readValue.length-1].amount + '</span></li>';
+					$scope.jsonStore = readValue;
+					$scope.$apply();
+					console.log(readValue);
 				}
 			});
-
 		});
 	}
+
+	$scope.getTotal = function(){
+		var total = 0;
+		for(var i = 0; i < $scope.jsonStore.length; i++){
+			total += parseInt($scope.jsonStore[i].amount);
+		}
+		return total;
+	}
+
+	$scope.saveEntry = function(){
+		console.log($scope.jsonStore);
+		if($scope.jsonStore==null) $scope.jsonStore=[];
+		$scope.value = {'desc':document.getElementById('txtEntry').value, 'amount':document.getElementById('numAmount').value};
+		$scope.jsonStore.push($scope.value);
+		console.log($scope.jsonStore);
+		localforage.setItem($scope.key, $scope.jsonStore, function() {
+			console.log('get item');
+			localforage.getItem($scope.key).then(function(readValue) {
+				$scope.jsonStore = readValue;
+			});
+		});
+	}
+});
